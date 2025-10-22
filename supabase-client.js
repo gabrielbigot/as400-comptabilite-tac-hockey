4aebf63c54bc1392f18f1d6b930e1d735b164c1c
gabrielbigot <157521895+gabrielbigot@users.noreply.github.com>
2025-10-22 18:38:26 +0200

    Update Supabase URL and anon key for production

--- supabase-client.js
+++ supabase-client.js
@@ -1,5 +1,5 @@
 console.log("supabase-client.js loaded");
-const SUPABASE_URL = 'http://127.0.0.1:54321';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0';
+const SUPABASE_URL = 'https://swsyvokuthjvgmezeodv.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3c3l2b2t1dGhqdmdtZXplb2R2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjExNDQxOTUsImV4cCI6MjA3NjcyMDE5NX0.-y-nVO34LnPpjIDkiqjQWHk0AiqsZWPKJKOWb4UYaVw';
 
 window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
